59f110b552c323226eee488930d17e1bab3dfa94
saqpler <86262550+saqpler@users.noreply.github.com>
2022-02-13 17:03:00 +0100

    Bot

---
+++ index.js
@@ -0,0 +1,52 @@
+﻿const Discord = require('discord.js');
+const fs = require('fs');
+const config = require('./config.json');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+var prefix = config.prefix;
+
+fs.readdir('./Commandes/', (error, f) => {
+    if (error) { return console.error(error); }
+        let commandes = f.filter(f => f.split('.').pop() === 'js');
+        if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
+
+        commandes.forEach((f) => {
+            let commande = require(`./Commandes/${f}`);
+            console.log(`${f} commandes chargées !`);
+            client.commands.set(commande.help.name, commande);
+        });
+});
+
+fs.readdir('./Events/', (error, f) => {
+    if (error) { return console.error(error); }
+        console.log(`${f.length} events chargés`);
+
+        f.forEach((f) => {
+            let events = require(`./Events/${f}`);
+            let event = f.split('.')[0];
+            client.on(event, events.bind(null, client));
+        });
+});
+
+client.login(config.token);
+
+  client.on('message', message =>{
+    if(!message.guild || message.author.bot == true) return;
+    if(message.content.toLowerCase() == "salefilsdeputedemerde"){
+      message.reply('ouep')
+    }
+  })
+
+
+
+const { GiveawaysManager } = require('discord-giveaways');
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaway.json",
+    updateCountdownEvery: 3000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: [ "MANAGE_MESSAGES", "ADMINISTRATOR" ],
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
